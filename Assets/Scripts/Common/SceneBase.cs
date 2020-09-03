1c7517a04d70459c79d3e79e992efecb1c804d2d
elesugiyama <sugiyama@elements-game.com>
2020-09-03 19:30:21 +0900

    20200903 switch版ゲームパッド用処理

--- Assets/Scripts/Common/SceneBase.cs
+++ Assets/Scripts/Common/SceneBase.cs
@@ -68,6 +68,9 @@ public class SceneBase : MonoBehaviour {
 				m_raycaster.enabled = true;
 			}
 		}
+#region KEY_TEST
+		StickAxisUpdate();
+#endregion //-*KEY_TEST
 	}
 	protected virtual void Awake() {
 		Debug.Log("//-*Awake:SceneBase"+(m_keepData!=null));
@@ -258,4 +261,122 @@ public class SceneBase : MonoBehaviour {
      }
 
 #endregion //-*OPTION
+
+#region GAME_PAD
+	public const float KEY_WAIT = 0.3f;	//-*キー入力後の受け付けない時間Delay
+	public const float KEY_AXIS_REFUSE_INCLINE = 0.5f;	//-*傾きの受付拒否幅
+	public Vector2 m_inputStick = Vector2.zero;
+	public bool m_isInputStick
+	{
+		set{}
+		get{return false;}
+
+	}
+	public float m_keyWaitTImeMax = 0.0f;	//-*キー入力後の受け付けない時間格納用
+	public float m_keyWaitTIme = 0.0f;	//-*キー入力後の受け付けない時間更新用
+	public enum KEY_NAME {
+		UP = 0,
+		DOWN,
+		LEFT,
+		RIGHT,
+		AXIS_MAX,	//-*方向キーここまで
+		SELECT,		//-*決定	//-* = AXIS_MAX,(代入式保留)
+		BACK,		//-*戻る
+		KEY_NAME_MAX,
+	};
+	public static Dictionary<KEY_NAME,string> GetPushKey = new Dictionary<KEY_NAME,string>()
+	{
+		{ KEY_NAME.BACK,	"joystick button 0" },	//-*戻るボタン
+		{ KEY_NAME.SELECT,	"joystick button 1" },	//-*決定ボタン
+	};
+
+	/// <summary>
+	/// ボタンが押されたか
+	/// </summary>
+	public bool IsKeyBtnPress(KEY_NAME keyName, bool isWait = true)
+	{
+		if(isWait && m_keyWaitTIme > 0 && keyName < KEY_NAME.AXIS_MAX)
+			return false;
+		if(Input.GetKeyDown(GetPushKey[keyName])){
+			return true;
+		}
+		return false;
+	}
+	/// <summary>
+	/// 方向キー入力
+	/// </summary>
+	public bool IsKeyAxisButton(KEY_NAME keyName, bool isWait = true)
+	{
+		bool isPress = false;
+		//-*受付拒否(受付拒否時間中,方向キー以外,傾きが一定値無い)
+		if(isWait && m_keyWaitTIme > 0)
+			return false;
+		if(keyName >= KEY_NAME.AXIS_MAX)
+			return false;
+		if( (keyName == KEY_NAME.UP || keyName == KEY_NAME.DOWN) && ( Mathf.Abs(m_inputStick.y) <= KEY_AXIS_REFUSE_INCLINE) )
+			return false;
+		if( (keyName == KEY_NAME.LEFT || keyName == KEY_NAME.RIGHT) && ( Mathf.Abs(m_inputStick.x) <= KEY_AXIS_REFUSE_INCLINE) )
+			return false;
+			
+		
+		int axisX = (m_inputStick.x > 0)? 1:-1;
+		int axisY = (m_inputStick.y > 0)? 1:-1;
+		switch(keyName){
+			case KEY_NAME.UP:
+				if(axisY == -1){
+					isPress = true;
+				}
+				break;
+			case KEY_NAME.DOWN:
+				if(axisY == 1){
+					isPress = true;
+				}
+				break;
+			case KEY_NAME.LEFT:
+				if(axisX == -1){
+					isPress = true;
+				}
+				break;
+			case KEY_NAME.RIGHT:
+				if(axisX == 1){
+					isPress = true;
+				}
+				break;
+			default:
+			break;
+		}
+		if(isWait){
+			m_keyWaitTImeMax = KEY_WAIT;
+		}
+		return isPress;
+	}
+	public void StickAxisUpdate(){
+
+		m_inputStick = Vector2.zero;
+
+		if( m_inputStick == Vector2.zero ) {
+			//外部コントローラー ジョイスティック
+			var x = Input.GetAxis( "Horizontal" );
+			var y = Input.GetAxis( "Vertical" );
+			if( Mathf.Abs( x ) <= 0.1f ) x = 0;
+			if( Mathf.Abs( y ) <= 0.1f ) y = 0;
+			if( x != 0 || y != 0 ) {
+				m_inputStick.x = x;
+				m_inputStick.y = y;
+				DebLog("//-****m_InputVector:"+m_inputStick);
+			}
+		}
+		//-*キー入力受付拒否時間
+		m_keyWaitTIme += Time.deltaTime;
+		var isTime = m_keyWaitTImeMax - m_keyWaitTIme;
+		
+		if(isTime < 0){
+			m_keyWaitTIme=0;
+			m_keyWaitTImeMax = 0;
+		}
+	}
+
+
+#endregion //-*GAME_PAD
+
 }
